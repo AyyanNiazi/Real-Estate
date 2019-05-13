3bf4c39f076b7f3e542c522dad1ee2cb672a8fff
AyyanNiazi <ayyanniazi82@gmail.com>
2019-05-13 18:39:47 +0500

    home almost done

--- src/component/home.js
+++ src/component/home.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 import home1 from './images/home1.jpg';
 import home2 from './images/home2.jpg';
 import home3 from './images/home3.jpg';
 import home4 from './images/home4.jpg';
+import hlogo1 from './images/hlogo1.png'
+import hlogo2 from './images/hlogo2.png'
+import hlogo3 from './images/hlogo3.png'
+import hlogo4 from './images/hlogo4.png'
+import hlogo5 from './images/hlogo5.png'
+import hlogo6 from './images/hlogo6.png'
+
 import './stylesheets/home.css'
 
 class Home extends Component {
@@ -10,7 +18,7 @@ class Home extends Component {
         super(props);
         this.state = {}
     }
-    
+
     render() {
         return (
             <div>
@@ -23,29 +31,205 @@ class Home extends Component {
                         </ol>
                         <div class="carousel-inner home-main ">
                             <div class="carousel-item active">
-                                <img class="d-block w-100 h-10" src={home4} alt="First slide"/>
+                                <img class="d-block w-100 h-10" src={home4} alt="First slide" />
                             </div>
-                           
-                                <div class="carousel-item">
-                                    <img class="d-block w-100 h-50" src={home2} alt="Second slide"/>
-                                </div>
-                                    <div class="carousel-item">
-                                        <img class="d-block w-100 h-100" src={home3} alt="Third slide"/>
-                                </div>
-                                    </div>
-                                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                        <span class="sr-only">Previous</span>
-                                    </a>
-                                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                        <span class="sr-only">Next</span>
-                                    </a>
-                                </div>
-                </section>
+
+                            <div class="carousel-item">
+                                <img class="d-block w-100 h-50" src={home2} alt="Second slide" />
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100 h-100" src={home3} alt="Third slide" />
+                            </div>
                         </div>
-                        );
-                   }
-               }
-                
+                        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                </section>
+
+                <main className='container' >
+                    <div className='sec-2' >
+                        <div className='row' >
+                            <div className='col-lg-8 col-xs-12 col-md-8 col-sm-12' >
+                                <h2 style={{ fontSize: '2.099em', fontWeight: '700', color: '#2e3e49' }} >Highly Customized Real Estate Website</h2><br />
+                                <p style={{ fontSize: '1.769em' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at fermentum felis. Phasellus eget vehicula sem. Duis malesuada sapien nec quam gravida accumsan. Nam vel quam in turpis ultrices </p>
+                            </div>
+                            <div className='col-lg-4 col-xs-12 col-md-4 col-sm-12' >
+                                <button type="button" class=" about-btn my-btn btn btn-outline-info">
+                                    <Link>About us<i class="fa fa-arrow-right"></i></Link>
+                                </button>
+                                {/* <button type="button" class=" my-btn btn btn-outline-warning">Warning</button> */}
+
+                            </div>
+                        </div>
+                    </div>
+                </main>
+
+                <section className='sec-3'>
+                    <div className='row' >
+                        <div className='new-home-div col-lg-4 col-xs-12 col-md-4 col-sm-12' >
+                            <h2 className='new-home'  >New<span >Homes</span></h2>
+                        </div>
+                        <div className='new-home-div-2 col-lg-4 col-xs-12 col-md-4 col-sm-12' >
+                            <h2 className='new-home'  >New<span >Homes</span></h2>
+                            <p className='new-home-p'>2 bed Rooms <br /> 4 bathrroms <br /> 1200 sqft</p>
+                        </div>
+                        <div className='new-home-div-3 col-lg-4 col-xs-12 col-md-4 col-sm-12' >
+                            <h2 className='new-home'  >Newdsd<span >Homes</span></h2><br />
+
+                        </div>
+                    </div>
+                </section>
+
+                <section className='sec-4 container' >
+                    <div>
+                        <h2 style={{ fontSize: '2.099em', fontWeight: '700', color: '#2e3e49' }} >Real Estate Services </h2><br />
+                        <p style={{ fontSize: '1.769em' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum<br /> at fermentum felis.</p>
+
+                    </div>
+                    <div style={{ marginTop: '150px' }} >
+                        <div className='row' >
+                            <div class='col-lg-4 col-xs-12 col-md-4 col-sm-12' >
+                                <div className='row' >
+                                    <div class='col-lg-2 col-sm-10'  >
+                                        <img src={hlogo1} />
+                                    </div>
+                                    <div class='col-lg-10 col-sm-10'  >
+                                        <h3>Enable Radius Search Functionality</h3><br />
+                                        <p>Search properties by their proximity to you in kilometers or miles on all search page</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class='col-lg-4 col-xs-12 col-md-4 col-sm-12' >
+                                <div className='row' >
+                                    <div class='col-lg-2 col-sm-2'>
+                                        <img src={hlogo2} />
+                                    </div>
+                                    <div class='col-lg-10 col-sm-10'>
+                                        <h3>Complete Searching Filter</h3><br />
+                                        <p>Allow your clients to fine-tune their search results and focus on the details that matter the most</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class='col-lg-4 col-xs-12 col-md-4 col-sm-12' >
+                                <div className='row' >
+                                    <div class='col-lg-2 col-sm-2'  >
+                                        <img src={hlogo3} />
+                                    </div>
+                                    <div class='col-lg-10 col-sm-10'  >
+                                        <h3>Advanced Search Options Panel</h3><br />
+                                        <p>Customize your search layout easily with a complete and advanced search option panel</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* @nd row  */}
+
+                        <div className='row' style={{ marginTop: '100px' }}  >
+                            <div class='col-lg-4 col-xs-12 col-md-4 col-sm-12' >
+                                <div className='row' >
+                                    <div class='col-lg-2 col-sm-10'  >
+                                        <img src={hlogo4} />
+                                    </div>
+                                    <div class='col-lg-10 col-sm-10'  >
+                                        <h3>Property Settings</h3><br />
+                                        <p>Create property listings with all the features you’d expect: area size, price range, amenities and more</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class='col-lg-4 col-xs-12 col-md-4 col-sm-12' >
+                                <div className='row' >
+                                    <div class='col-lg-2 col-sm-2'>
+                                        <img src={hlogo5} />
+                                    </div>
+                                    <div class='col-lg-10 col-sm-10'>
+                                        <h3>Compare Properties</h3><br />
+                                        <p>Let your users compare different properties based on their features and parameters</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class='col-lg-4 col-xs-12 col-md-4 col-sm-12' >
+                                <div className='row' >
+                                    <div class='col-lg-2 col-sm-2'  >
+                                        <img src={hlogo6} />
+                                    </div>
+                                    <div class='col-lg-10 col-sm-10'  >
+                                        <h3>Agent Contact Forms</h3><br />
+                                        <p>Help your agents make sales by placing an easy-to-use contact form in the header of each listing</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                <section class='sec-5 container ' >
+                    <div className='row' >
+                        <div className='col-lg-8 col-xs-12 col-md-8 col-sm-12' >
+                            <h2 style={{ fontSize: '2.099em', fontWeight: '700', color: '#2e3e49' }} >Featured Properties</h2><br />
+                            <p style={{ fontSize: '1.769em' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at fermentum felis. </p>
+                        </div>
+                        <div className='col-lg-4 col-xs-12 col-md-4 col-sm-12' >
+                            <button type="button" class=" property-btn my-btn btn btn-outline-info">
+                                <Link>View All properties<i class="fa fa-arrow-right"></i></Link>
+                            </button>
+                            {/* <button type="button" class=" my-btn btn btn-outline-warning">Warning</button> */}
+
+                        </div>
+                    </div>
+                    {/* 2nd */}
+                    <div className='row' style={{ marginTop: '30px' }} >
+                        <div className=' feature-1 col-lg-7 col-sm-12 col-xs-12' >
+                            <div class='feature-1-div' >
+                                <ul className='actions' >
+                                    <li>h</li>
+                                    <li>h</li>
+                                    <li>h</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className=' feature-2 col-lg-5 col-sm-12 col-xs-12' >
+
+                        </div>
+                    </div>
+                    <div className='row' >
+                        <div className=' feature-3 col-lg-5 col-sm-12 col-xs-12' >
+                            <div class='feature-1-div' >
+                                <ul className='actions' >
+                                    <li>h</li>
+                                    <li>h</li>
+                                    <li>h</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className=' feature-4 col-lg-7 col-sm-12 col-xs-12' >
+
+                        </div>
+                    </div>
+
+                    {/* stay in touch with us */}
+                    <div className='row' >
+                        <div className='col-lg-4 col-xs-12 col-sm-12 ' > 
+                            <h4> Stay in Touch With us </h4>
+                        </div>
+                        <form className='col-lg-8 col-xs-12 col-sm-12 '   >
+                            <div class="form-group">
+                                
+                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+    
+                            </div>
+                        </form>
+                    </div>
+                    </section>
+            </div>
+                );
+            }
+        }
+        
 export default Home;
